301e109c27fbf064f14925ae7149654d794106a6
Patrick Fitzsimmons <pat@pfitz.net>
2017-05-24 00:29:48 -0400

    adding mobile auth points

--- java-app/src/main/java/io/stallion/clubhouse/AuthEndpoints.java
+++ java-app/src/main/java/io/stallion/clubhouse/AuthEndpoints.java
@@ -5,9 +5,12 @@ import java.util.Map;
 
 import static io.stallion.utils.Literals.*;
 
+import io.stallion.requests.Site;
 import io.stallion.restfulEndpoints.BodyParam;
 import io.stallion.restfulEndpoints.EndpointResource;
+import io.stallion.restfulEndpoints.XSRF;
 import io.stallion.services.Log;
+import io.stallion.settings.Settings;
 import io.stallion.users.IUser;
 import io.stallion.users.UserController;
 
@@ -33,6 +36,33 @@ public class AuthEndpoints implements EndpointResource {
                 val(
                         "user", user
                 ),
+
+                val("defaultChannelId", ChannelController.instance().getFirstUserChannel(user.getId())),
+                val("userProfile", up)
+        );
+    }
+
+    @POST
+    @Path("/mobile-login")
+    @XSRF(false)
+    public Object mobileLogin(@BodyParam("username") String username, @BodyParam("password") String password) {
+        if (username.contains("@")) {
+            IUser fromEmail = UserController.instance().forEmail(username);
+            if (fromEmail != null) {
+                username = fromEmail.getUsername();
+            }
+        }
+        IUser user = UserController.instance().checkUserLoginValid(username, password);
+        String cookie =  UserController.instance().userToCookieString(user, true, null);
+        UserProfile up = UserProfileController.instance().forStallionUserOrNotFound(user.getId());
+        return map(
+                val(
+                        "user", user
+                ),
+                val("authcookie", cookie),
+                val(
+                        "site", new Site().setName(Settings.instance().getSiteName())
+                        ),
                 val("defaultChannelId", ChannelController.instance().getFirstUserChannel(user.getId())),
                 val("userProfile", up)
         );
